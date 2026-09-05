1895585a3abf1fde0ee5a8784c9e4c4089cae8b9
Shahnewaz Alam <181187140+heysanzu@users.noreply.github.com>
2026-09-05 09:52:56 +0530

    Update note title and add new entry

--- shielder/notes.js
+++ shielder/notes.js
@@ -1,7 +1,12 @@
 const NOTES = [
    {
-     title: "Computer",
+     title: "Computer: Shortcuts keys",
      fileName: "shortcuts-keys.pdf",
      data: "src/notes/shortcuts-keys.pdf",
+   },
+   {
+      title: "English: Regular and Irregular verbs",
+      fileName: "regular-Irregular-verbs.pdf",
+      data: "src/notes/regular-Irregular-verbs.pdf",
    },
 ];
