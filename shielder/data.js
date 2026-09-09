bad15df1711bd2d5bd81665533db0c307b0f69ed
Shahnewaz Alam <181187140+heysanzu@users.noreply.github.com>
2026-09-09 11:01:23 +0530

    Record attendance for students on September 4, 2026
    Added attendance records for specific students on September 4, 2026.

--- shielder/data.js
+++ shielder/data.js
@@ -224,6 +224,11 @@ const STUDENTS = {
 Object.values(STUDENTS).forEach(student => {
   student.attendance["2026-09-01"] = "present";
 });
+STUDENT_01.attendance["2026-09-04"] = "absent";
+STUDENT_02.attendance["2026-09-04"] = "absent";
+STUDENT_06.attendance["2026-09-04"] = "absent";
+STUDENT_07.attendance["2026-09-04"] = "absent";
+STUDENT_10.attendance["2026-09-04"] = "absent";
 
 Object.values(STUDENTS).forEach(student => {
   student.attendance["2026-09-02"] = "present";
