22712bc9695f07ee37004dc4e2951b58f6ac6473
Shahnewaz Alam <181187140+heysanzu@users.noreply.github.com>
2026-09-09 11:05:21 +0530

    Update attendance date for STUDENT_10

--- shielder/data.js
+++ shielder/data.js
@@ -262,7 +262,7 @@ STUDENT_02.attendance["2026-09-05"] = "absent";
 STUDENT_05.attendance["2026-09-05"] = "absent";
 STUDENT_06.attendance["2026-09-05"] = "absent";
 STUDENT_07.attendance["2026-09-05"] = "absent";
-STUDENT_10.attendance["2026-09-04"] = "absent";
+STUDENT_10.attendance["2026-09-05"] = "absent";
 
 Object.values(STUDENTS).forEach(student => {
   student.attendance["2026-09-07"] = "present";
